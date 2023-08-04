359dfe92a55b388f4a6e6160182743f209eb8245
demiakinsola <oluwademiladeakinsola@gmail.com>
2023-08-04 04:00:30 +0100

    Fixed file tree

--- Backend/routes/handleLoginRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { handleLogin } = require("../controller/handleLoginController");
-
-router.post("/", handleLogin);
-
-module.exports = router;
